c9d506c7c12903afa39fc109e0cec76cf0299207
cristian.lastrucci <cristian.lastrucci@gmail.com>
2018-11-13 17:29:42 +0100

    inizializzato oreamnos-services

---
+++ oreamnos-backend/oreamnos-services/src/main/java/clast/oreamnos/services/App.java
@@ -0,0 +1,13 @@
+package clast.oreamnos.services;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( sayHello() );
+    }
+
+	public static String sayHello() {
+		return "Hello World!";
+	}
+}
---
+++ oreamnos-backend/oreamnos-services/src/test/java/clast/oreamnos/services/AppTest.java
@@ -0,0 +1,14 @@
+package clast.oreamnos.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testSayHello() {
+		assertEquals( "Hello World!", App.sayHello() );
+	}
+
+}
